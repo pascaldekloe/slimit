d6cff519aecd4fc032156a8f0e36fd6e115a96d6
Pascal S. de Kloe <pascal@quies.net>
2020-04-28 19:29:54 +0200

    Eliminate panic cases (for speed).

--- slimit.go
+++ slimit.go
@@ -8,9 +8,34 @@ package slimit
 // The concatenation of head and remainder equals s for any i.
 // Head is empty when i ≤ 0. Remainder is empty when i ≥ len(s).
 func SplitAt(s string, i int) (head, remainder string) {
-	head = NBytes(s, i)
-	return head, s[len(head):]
+	if len(s) <= i {
+		return s, ""
+	}
+	if i <= 0 {
+		return "", s
+	}
+	if isTail(s[i]) {
+		// cut-off in multi-byte sequence
+		for j := i - 1; j >= 0; j-- {
+			if isTail(s[j]) {
+				continue
+			}
+
+			// Check whether the tail actually reaches s[n].
+			// The most significant bits are 0b110… with 2 byte
+			// sequences, 0b1110… with 3 and 0b11110… with 4.
+			if byte(int8(-128)>>uint(i-j)) <= s[j] {
+				return s[:j], s[j:]
+			}
+			break
+		}
+		// broken sequence
+	}
+	return s[:i], s[i:]
 }
+
+// isTail returns whether c is a continuation of a muli-byte sequence.
+func isTail(c byte) bool { return c>>6 == 0b10 }
 
 // NBytes limits the string to a maximum number of bytes.
 // If a cut-off would interrupt a UTF-8 byte sequence, then the
@@ -20,31 +45,6 @@ func SplitAt(s string, i int) (head, remainder string) {
 // The return is empty when n ≤ 0.
 // The return is s when n ≥ len(s).
 func NBytes(s string, n int) string {
-	if len(s) <= n {
-		return s
-	}
-	if n <= 0 {
-		return ""
-	}
-	if isTail(s[n]) {
-		// cut-off in multi-byte sequence
-		for i := n - 1; i >= 0; i-- {
-			if isTail(s[i]) {
-				continue
-			}
-
-			// Check whether the tail actually reaches s[n].
-			// The most significant bits are 0b110… with 2 byte
-			// sequences, 0b1110… with 3 and 0b11110… with 4.
-			if byte(int8(-128)>>(n-i)) <= s[i] {
-				return s[:i]
-			}
-			break
-		}
-		// broken sequence
-	}
-	return s[:n]
+	head, _ := SplitAt(s, n)
+	return head
 }
-
-// isTail returns whether c is a continuation of a muli-byte sequence.
-func isTail(c byte) bool { return c>>6 == 0b10 }
