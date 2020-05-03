1c0277a52453a4c429ad6f9f7b2ae03ccc75e05a
Pascal S. de Kloe <pascal@quies.net>
2020-05-03 18:23:31 +0200

    Validate tail encoding. This is to ensure consistency with core library, which counts illegal UTF-8 bytes as one rune each.

--- slimit.go
+++ slimit.go
@@ -1,6 +1,8 @@
 // Package slimit provides size boundaries on UTF-8 data.
 package slimit
 
+import "unicode/utf8"
+
 // SplitAt cuts on index i, i.e., s is separated after i bytes.
 // If a splitup would interrupt a UTF-8 byte sequence, then the
 // incomplete tail (from head) goes to remainder.
@@ -8,7 +10,7 @@ package slimit
 // The concatenation of head and remainder equals s for any i.
 // Head is empty when i ≤ 0. Remainder is empty when i ≥ len(s).
 func SplitAt(s string, i int) (head, remainder string) {
-	if len(s) <= i {
+	if i >= len(s) {
 		return s, ""
 	}
 	if i <= 0 {
@@ -16,18 +18,17 @@ func SplitAt(s string, i int) (head, remainder string) {
 	}
 	if isTail(s[i]) {
 		// cut-off in multi-byte sequence
-		for j := i - 1; j >= 0; j-- {
-			if isTail(s[j]) {
-				continue
+		j := i - 1
+		if j > 0 && isTail(s[j]) {
+			j--
+			if j > 0 && isTail(s[j]) {
+				j--
+				// encoding is limited to 4 bytes
 			}
-
-			// Check whether the tail actually reaches s[n].
-			// The most significant bits are 0b110… with 2 byte
-			// sequences, 0b1110… with 3 and 0b11110… with 4.
-			if byte(int8(-128)>>uint(i-j)) <= s[j] {
-				return s[:j], s[j:]
-			}
-			break
+		}
+		_, size := utf8.DecodeRuneInString(s[j:])
+		if size > i-j {
+			return s[:j], s[j:]
 		}
 		// broken sequence
 	}
